9109ec85b97fbeb5f7cdac9478fadf8d5570e488
Gnana Prakash M <gnanaprakash2918@gmail.com>
2025-11-25 18:55:53 +0530

    Add Path checks to extractor

--- core/processing/extractor.py
+++ core/processing/extractor.py
@@ -4,10 +4,43 @@ from collections.abc import Generator
 class FrameExtractor:
     def extract(video_path: str | Path, interval: int = 2) -> Generator[Path, None, None]:
         # Convert a video file into sequence of frames at regular intervals
-        if isinstance(video_path, str):
-            if video_path.strip() == '':
-                return
+        # interval=2 means "Take a screenshot every 2 seconds."
 
-            path_obj = Path(video_path)
-        else:
-            path_obj = video_path
+        try:
+            if isinstance(video_path, str):
+                if video_path.strip() == '':
+                    raise ValueError("Provided path is empty or whitespace.")
+
+                path_obj = Path(video_path)
+            else:
+                path_obj = video_path
+
+            if not path_obj.exists():
+                raise FileNotFoundError(
+                    f"Path does not exist: {video_path}"
+                )
+
+            if not path_obj.is_dir():
+                raise IsADirectoryError(
+                    f"Path is not a directory: {path_obj}"
+                )
+        except (
+                ValueError,
+                NotADirectoryError,
+                FileNotFoundError,
+                PermissionError,
+                IsADirectoryError,
+                OSError,
+        ) as exc:
+            print(
+                f"[ERROR:{type(exc).__name__}] "
+                f"Cannot read '{video_path}': {exc}"
+            )
+            return
+
+        except Exception as exc:  # pylint: disable=broad-except
+            print(
+                f"[ERROR:{type(exc).__name__}] "
+                f"Cannot read '{video_path}': {exc}"
+            )
+            return
